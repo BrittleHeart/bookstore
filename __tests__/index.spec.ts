17361012141792dbc92eb85a9a25d3399b78ed79
Bartosz <b.pazdur@yahoo.com>
2022-06-26 15:10:17 +0200

    Bug fixing and improvements
    Fix invalid tests loading, eslint config and prettier

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,10 +1,10 @@
-import Application from "@/index"
+import Application from '@/index'
 
-describe('Application', () => { 
-  describe('Run Application', () => { 
+describe('Application', () => {
+  describe('Run Application', () => {
     it('should have defined runApp method', () => {
-      expect(Application).toBeDefined();
-      expect(new Application().runApp).toBeDefined();
+      expect(Application).toBeDefined()
+      expect(new Application().runApp).toBeDefined()
     })
-  })  
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 export default class Application {
-  constructor() {}
-
   async runApp(): Promise<void> {
-    console.log('Hi application');
+    console.log('Hi application')
   }
 }
 
-const app = '1';
-
 async function bootstrap() {
-  const app = new Application();
+  const app = new Application()
 
   await app.runApp()
 }
